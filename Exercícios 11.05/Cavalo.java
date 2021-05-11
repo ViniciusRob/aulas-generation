ddba862f8b34b0c94dceb9143fd5bc0a298d663a
Vinicius Roberto <62081027+ViniciusRob@users.noreply.github.com>
2021-05-11 11:00:19 -0300

    Add files via upload

---
+++ Exercícios 11.05/Cavalo.java
@@ -0,0 +1,14 @@
+package br.com.exercicio;
+
+public class Cavalo extends Cachorro{
+	
+	private double tamanho;
+
+	public double getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+}

0421c81188dee4498754a4d3594d4f454fb831f7
Vinicius Roberto <62081027+ViniciusRob@users.noreply.github.com>
2021-05-11 10:43:59 -0300

    Add files via upload
    Exercícios do dia 11/05

---
+++ Exercicio 11.05/Preguica.java
@@ -0,0 +1,18 @@
+package br.com.exercicio;
+
+public class Preguica extends Cachorro{
+
+	private String alimentos;
+
+	public String getAlimentos() {
+		return alimentos;
+	}
+
+	public void setAlimentos(String alimentos) {
+		this.alimentos = alimentos;
+	}
+	
+	void subirArvore() {
+		
+	}
+}
